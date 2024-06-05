364df6f7ab1c38fba154eaedfcd70f5d9c9dba13
Sakthivel G <112932102+sakthiofficial@users.noreply.github.com>
2024-06-05 15:59:02 +0530

    Update layout.js
    remove snapbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { Provider } from "react-redux";
-import { SnackbarProvider } from "notistack";
+
 import { ThemeProvider } from "@mui/material/styles";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,11 +16,11 @@ export default function RootLayout({ children }) {
   return (
     <Provider store={store}>
       <ThemeProvider theme={themeFont}>
-        <SnackbarProvider maxSnack={3}>
+       
           <html lang="en">
             <body className={inter.className}>{children}</body>
           </html>
-        </SnackbarProvider>
+       
       </ThemeProvider>
     </Provider>
   );
